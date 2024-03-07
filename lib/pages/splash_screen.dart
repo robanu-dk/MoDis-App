87bef402eda20e899358119dca51d9194aa41beb
robanu <bestrobanu@gmail.com>
2024-03-07 23:36:19 +0700

    update: splash screen (change bg color) and remove connectivity check

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:modis/pages/login.dart';
 
@@ -12,111 +11,66 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  bool _start = true;
-  late Timer _timer;
-
   @override
   void initState() {
     super.initState();
-
-    checkInternetConnection(context);
-
-    try {
-      Connectivity().onConnectivityChanged.listen((event) {
-        if (event == ConnectivityResult.wifi ||
-            event == ConnectivityResult.mobile) {
-          if (!_start) {
-            ScaffoldMessenger.of(context).removeCurrentSnackBar();
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Center(child: Text('Terhubung Internet')),
-                backgroundColor: Colors.green,
-                duration: Duration(seconds: 8),
-              ),
-            );
-          }
-
-          _timer = Timer(const Duration(seconds: 10), () {
-            Navigator.pushReplacement(
-              context,
-              PageRouteBuilder(
-                pageBuilder: (context, anitamtion, secondaryAnimation) =>
-                    const LoginPage(),
-                transitionsBuilder:
-                    (context, animation, secondaryAnimation, child) =>
-                        FadeTransition(
-                  opacity: animation,
-                  child: child,
-                ),
-              ),
-            );
-          });
-        } else {
-          _timer.cancel();
-          ScaffoldMessenger.of(context).removeCurrentSnackBar();
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(
-              content: Center(child: Text('Tidak Terhubung Internet')),
-              backgroundColor: Colors.redAccent,
-              duration: Duration(minutes: 5),
-            ),
-          );
-          if (_start) {
-            setState(() {
-              _start = false;
-            });
-          }
-        }
-      });
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  Future<void> checkInternetConnection(context) async {
-    try {
-      final connectivityResult = await (Connectivity().checkConnectivity());
-      if (connectivityResult != ConnectivityResult.wifi &&
-          connectivityResult != ConnectivityResult.mobile) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(
-            content: Center(child: Text('Tidak Terhubung Internet')),
-            backgroundColor: Colors.redAccent,
-            duration: Duration(minutes: 5),
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (context, anitamtion, secondaryAnimation) =>
+              const LoginPage(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+              FadeTransition(
+            opacity: animation,
+            child: child,
           ),
-        );
-        if (_start) {
-          setState(() {
-            _start = false;
-          });
-        }
-      }
-    } catch (e) {
-      print(e);
-    }
+        ),
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'images/logo.png',
-              height: MediaQuery.of(context).size.height * 0.3,
-            ),
-            const Text(
-              'MoDis',
-              style: TextStyle(
-                fontSize: 50,
-                fontFamily: 'italianno',
-                fontWeight: FontWeight.w200,
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            colors: [
+              Color.fromRGBO(1, 92, 104, 1),
+              Color.fromRGBO(1, 135, 76, 1),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                ),
+                child: Image.asset(
+                  'images/logo.png',
+                  height: 200,
+                ),
               ),
-            ),
-          ],
+              const Text(
+                'MoDis',
+                style: TextStyle(
+                    fontSize: 70,
+                    fontFamily: 'italianno',
+                    fontWeight: FontWeight.w200,
+                    color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
     );
