34111c0b46eb454a2d442deb2558220967296623
robanu <bestrobanu@gmail.com>
2024-03-19 00:45:45 +0700

    create: component alert input

---
+++ lib/components/alert_input_implement.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class AlertInput extends StatelessWidget {
+  const AlertInput({
+    super.key,
+    required this.height,
+    required this.header,
+    required this.headerPadding,
+    required this.contents,
+    required this.contentAligment,
+    required this.contentPadding,
+    required this.actionAligment,
+    required this.actions,
+    required this.actionPadding,
+  });
+
+  final Widget header;
+  final List<Widget> contents, actions;
+  final String actionAligment, contentAligment;
+  final double height;
+  final EdgeInsetsGeometry headerPadding, contentPadding, actionPadding;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.8,
+        height: height,
+        child: Column(
+          children: [
+            Padding(
+              padding: headerPadding,
+              child: header,
+            ),
+            Padding(
+              padding: contentPadding,
+              child: contentAligment == 'vertical'
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: contents,
+                    )
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: contents,
+                    ),
+            ),
+            Padding(
+              padding: actionPadding,
+              child: actionAligment == 'vertical'
+                  ? Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: actions,
+                    )
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: actions,
+                    ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
