138ea30c12f3db490d6b551df29f151f48a98032
robanu <bestrobanu@gmail.com>
2024-03-20 13:02:39 +0700

    install icon and done create custom bottom navigation button

---
+++ lib/components/custom_navigation_bar.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:icons_flutter/icons_flutter.dart';
+import 'package:modis/pages/beranda.dart';
+import 'package:modis/pages/chat.dart';
+import 'package:modis/pages/list_account.dart';
+import 'package:modis/pages/profile.dart';
+import 'package:modis/pages/video.dart';
+import 'package:modis/providers/user.dart';
+import 'package:provider/provider.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  const CustomBottomNavigationBar({super.key, required this.currentIndex});
+
+  final int currentIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> superMenu = [
+      BottomNavigationItem(
+        index: 0,
+        currentIndex: currentIndex,
+        icon: Icons.dataset_outlined,
+        label: 'Beranda',
+        action: () {
+          pushReplacement(context, const Beranda());
+        },
+      ),
+      BottomNavigationItem(
+        index: 1,
+        currentIndex: currentIndex,
+        icon: Icons.supervised_user_circle_rounded,
+        label: 'List Akun',
+        action: () {
+          pushReplacement(context, const ListAccount());
+        },
+      ),
+      BottomNavigationItem(
+        index: 2,
+        currentIndex: currentIndex,
+        icon: Icons.play_circle_outline,
+        label: 'Video',
+        action: () {
+          pushReplacement(context, const Video());
+        },
+      ),
+      BottomNavigationItem(
+        index: 3,
+        currentIndex: currentIndex,
+        icon: Ionicons.ios_chatbubbles,
+        label: 'Chat',
+        action: () {
+          pushReplacement(context, const Chat());
+        },
+      ),
+      BottomNavigationItem(
+        index: 4,
+        currentIndex: currentIndex,
+        icon: Icons.account_circle,
+        label: 'Profil',
+        action: () {
+          pushReplacement(context, const Profile());
+        },
+      ),
+    ];
+
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromRGBO(0, 0, 0, 0.25),
+            offset: Offset(0, -2),
+          ),
+        ],
+      ),
+      height: 61,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: superMenu,
+      ),
+    );
+  }
+
+  pushReplacement(BuildContext context, destination) {
+    Navigator.pushReplacement(
+      context,
+      PageRouteBuilder(
+        transitionDuration: const Duration(milliseconds: 200), // Durasi animasi
+        pageBuilder: (BuildContext context, Animation<double> animation,
+            Animation<double> secondaryAnimation) {
+          return destination;
+        },
+        transitionsBuilder: (BuildContext context, Animation<double> animation,
+            Animation<double> secondaryAnimation, Widget child) {
+          return FadeTransition(
+            opacity: animation,
+            child: child,
+          );
+        },
+      ),
+    );
+  }
+}
+
+class BottomNavigationItem extends StatelessWidget {
+  const BottomNavigationItem({
+    super.key,
+    required this.index,
+    required this.currentIndex,
+    required this.icon,
+    required this.label,
+    required this.action,
+  });
+
+  final int currentIndex, index;
+  final Function() action;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: action,
+      icon: currentIndex == index
+          ? Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(1, 98, 104, 1),
+                    Color.fromRGBO(1, 135, 76, 1)
+                  ],
+                ),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    icon,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    label,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+            )
+          : Icon(
+              icon,
+              color: const Color.fromARGB(255, 105, 105, 105),
+            ),
+    );
+  }
+}
