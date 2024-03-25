8ef97ffccff48f737c762c7ef030ef82618aac84
robanu <bestrobanu@gmail.com>
2024-03-26 03:48:37 +0700

    create: component circle button

---
+++ lib/components/circle_button.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CircleButtonModis extends StatelessWidget {
+  const CircleButtonModis({
+    super.key,
+    required this.icon,
+    required this.label,
+    required this.colors,
+    required this.margin,
+    required this.onPressed,
+  });
+
+  final IconData icon;
+  final String label;
+  final List<Color> colors;
+  final EdgeInsetsGeometry margin;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: margin,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(
+                Radius.circular(100),
+              ),
+              gradient: LinearGradient(
+                colors: colors,
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+            child: IconButton(
+              onPressed: onPressed,
+              icon: Icon(
+                icon,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Text(label),
+        ],
+      ),
+    );
+  }
+}
