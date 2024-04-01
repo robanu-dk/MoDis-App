290bbae177d4db634f72b2398ff87fcbbfe9079a
robanu <bestrobanu@gmail.com>
2024-04-01 11:44:47 +0700

    add: get list available account

--- lib/providers/child.dart
+++ lib/providers/child.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class Child extends ChangeNotifier {
   String email = '', token = '';
-  dynamic listChild;
+  dynamic listChild, allAvailableChild;
   String apiDomain = 'http://10.0.2.2:8080/API/Modis/public/api/guide';
 
   void updateUser(String email, String token) {
@@ -14,14 +14,14 @@ class Child extends ChangeNotifier {
     notifyListeners();
   }
 
-  getListChild({String? filter}) {
+  search({dynamic data, String? filter}) {
     if (filter != '') {
-      return listChild
+      return data
           .where((element) =>
               element['username'].toString().contains(filter.toString()))
           .toList();
     } else {
-      return listChild;
+      return data;
     }
   }
 
@@ -48,6 +48,70 @@ class Child extends ChangeNotifier {
       if (response['status'] == 'success') {
         listChild = response['data'];
         notifyListeners();
+      }
+
+      return response;
+    } catch (error) {
+      throw error.toString();
+    }
+  }
+
+  getListAvailableChild() async {
+    try {
+      Uri url = Uri.parse('$apiDomain/all-user');
+
+      var post = await http.post(
+        url,
+        headers: {
+          'Content-Type': 'application/json',
+          'authorization': 'Bearer $token',
+        },
+        body: jsonEncode(
+          {
+            'email': email,
+          },
+        ),
+      );
+
+      var response = jsonDecode(post.body);
+
+      if (response['status'] == 'success') {
+        allAvailableChild = response['data'];
+        notifyListeners();
+      }
+    } catch (error) {
+      throw error.toString();
+    }
+  }
+
+  Future<dynamic> createNewChildAccount(
+    String name,
+    String username,
+    String userEmail,
+    int? gender,
+    String password,
+  ) async {
+    try {
+      Uri url = Uri.parse('$apiDomain/create-user');
+
+      var post = await http.post(url,
+          headers: {
+            'Content-Type': 'application/json',
+            'authorization': 'Bearer $token',
+          },
+          body: jsonEncode({
+            'guide_email': email,
+            'user_email': userEmail,
+            'name': name,
+            'username': username,
+            'jenis_kelamin': gender,
+            'password': password,
+          }));
+
+      var response = jsonDecode(post.body);
+
+      if (response['status'] == 'success') {
+        getListData();
       }
 
       return response;
