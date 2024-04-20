4f4f94f68861d542654f4816285d491e524622e7
robanu <bestrobanu@gmail.com>
2024-04-20 15:20:53 +0700

    adjust: add controller parameter

--- lib/components/search_input.dart
+++ lib/components/search_input.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
 class SearchModis extends StatelessWidget {
-  const SearchModis({super.key, required this.onSubmitted, this.focusNode});
+  const SearchModis(
+      {super.key, required this.onSubmitted, this.focusNode, this.controller});
   final ValueChanged onSubmitted;
   final FocusNode? focusNode;
+  final TextEditingController? controller;
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       onSubmitted: onSubmitted,
       focusNode: focusNode,
+      controller: controller,
       textInputAction: TextInputAction.search,
       decoration: const InputDecoration(
         border: OutlineInputBorder(
