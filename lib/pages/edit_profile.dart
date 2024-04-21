667f4b8c2114795c69bf5306281f6b8a1bb92a9b
robanu <bestrobanu@gmail.com>
2024-04-21 19:47:50 +0700

    adjust: improve view based physic emulator

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -170,7 +170,7 @@ class _EditProfileState extends State<EditProfile> {
                                   builder: (context, user, child) =>
                                       user.userProfileImage != ''
                                           ? Image.network(
-                                              'http://10.0.2.2:8080/API/Modis/public/${user.userProfileImage}',
+                                              'http://192.168.42.60:8080/API/Modis/public/${user.userProfileImage}',
                                               fit: BoxFit.cover,
                                               filterQuality: FilterQuality.high,
                                             )
