d410886ad13931dbc7ef62256a6da89d5c2dedf5
robanu <bestrobanu@gmail.com>
2024-05-15 01:56:38 +0700

    adjust: improve outlinedButtonModis component

--- lib/components/outline_button_implement.dart
+++ lib/components/outline_button_implement.dart
@@ -1,27 +1,48 @@
 import 'package:flutter/material.dart';
 
 class OutlinedButtonModis extends StatelessWidget {
-  const OutlinedButtonModis(
-      {super.key, required this.childrens, required this.onPressed});
+  const OutlinedButtonModis({
+    super.key,
+    required this.childrens,
+    required this.onPressed,
+    this.margin = const EdgeInsets.only(left: 14.0, right: 14.0, bottom: 6.0),
+    this.contentPadding,
+    this.radius = 8.0,
+  });
 
   final List<Widget> childrens;
   final VoidCallback onPressed;
+  final EdgeInsets margin;
+  final EdgeInsets? contentPadding;
+  final double radius;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(left: 14.0, right: 14.0, bottom: 6.0),
+      margin: margin,
       child: OutlinedButton(
-        style: const ButtonStyle(
-          alignment: Alignment.centerLeft,
-          side: MaterialStatePropertyAll(
-              BorderSide(color: Color.fromARGB(255, 123, 123, 123))),
-          shape: MaterialStatePropertyAll(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            ),
-          ),
-        ),
+        style: contentPadding == null
+            ? ButtonStyle(
+                alignment: Alignment.centerLeft,
+                side: const MaterialStatePropertyAll(
+                    BorderSide(color: Colors.black)),
+                shape: MaterialStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(radius)),
+                  ),
+                ),
+              )
+            : ButtonStyle(
+                padding: MaterialStatePropertyAll(contentPadding),
+                alignment: Alignment.centerLeft,
+                side: const MaterialStatePropertyAll(
+                    BorderSide(color: Colors.black)),
+                shape: MaterialStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(radius)),
+                  ),
+                ),
+              ),
         onPressed: onPressed,
         child: Row(
           children: childrens,
