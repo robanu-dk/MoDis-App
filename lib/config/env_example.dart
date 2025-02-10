389e0ccbff4bf8291dde6312d7de78cd643c5c10
robanu <rd.scout06@gmail.com>
2025-02-10 22:57:50 +0700

    add: env example

---
+++ lib/config/env_example.dart
@@ -0,0 +1,6 @@
+// remove '_example' from the file name to use this file
+
+class Env {
+  static const String apiUrl = 'https://api.example.com';
+  static const String assetUrl = 'https://api.example.com';
+}
